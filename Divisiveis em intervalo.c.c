14b3ff68ddefca54e8c1af4f24fdfcb4d85bb782
LucasGabriel <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-05 12:33:10 -0300

    Update Divisiveis-em-intervalo.c.c

--- Divisiveis em intervalo.c.c
+++ Divisiveis em intervalo.c.c
@@ -1,13 +1,11 @@
 
-/*
-Autor...: LUCAS GABRIEL DOS SANTOS LIMA 
+/*Autor...: LUCAS GABRIEL DOS SANTOS LIMA 
 Data....: 10/05/2024
-Objetivo: Imprimir os numeros divisiveis por 4 menores que 200.
-*/
+Objetivo: Imprimir os numeros divisiveis por 4 menores que 200.*/
 
 #include <stdio.h>
-
 int main() {
+    //Definicao de variaveis  cont = contador 
     int cont = 1;
 
     printf("Numeros divisiveis por 4 e menores que 200:");
@@ -17,6 +15,6 @@ int main() {
         }
         cont++;
     }
-
     return 0;
 }
+//✟
