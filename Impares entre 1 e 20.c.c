3158982813735f6c9d0fb5d09b49b37dbcb15fbc
LucasGabriel <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-05 12:40:38 -0300

    Update Impares entre 1 e 20.c.c

--- Impares entre 1 e 20.c.c
+++ Impares entre 1 e 20.c.c
@@ -1,12 +1,10 @@
-
-/*
-Author..: LUCAS GABRIEL DOS SANTOS LIMA <contabtclucas@gmail.com>
-Date......: 10/05/2024
-OB..........: Imprimir os valores ímpares entre 0 e 20.
-*/
+/*Author..: LUCAS GABRIEL DOS SANTOS LIMA <contabtclucas@gmail.com>
+  Date......: 10/05/2024
+  OB..........: Imprimir os valores impares entre 0 e 20.*/
 
 #include <stdio.h>
 int main() {
+    //Definicao de variaveis     i = impares ( tambem usado como contador no laco "for')
     int i;
     for ( i = 0; i <=20; i++){
         if (i % 2 != 0){
