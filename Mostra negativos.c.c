cff1b2b80ede1db0b28a742201b46c6d3686b52b
LucasGabriel <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-05 12:46:29 -0300

    Update Mostra-negativos.c.c

--- Mostra negativos.c.c
+++ Mostra negativos.c.c
@@ -1,24 +1,23 @@
-
-/*
-Autor...: LUCAS GABRIEL DOS SANTOS LIMA 
-Data....: 10/05/2024
-Objetivo: Contar e mostrar o total de valores negativos inseridos pelo usuário.
-*/
+/*Autor...: LUCAS GABRIEL DOS SANTOS LIMA 
+  Data....: 10/05/2024
+  Objetivo: Contar e mostrar o total de valores negativos inseridos pelo usuário.*/
 
 #include <stdio.h>
 int main() {
-    int a, i, cn = 0;
+    //Defnicao de variaveis    nn = Numero negativo    i = contador do laco de repeticao (variavel de controle)
+    //cn = contador de negativos 
+    
+    int nn, i, cn = 0;
 
     for (i = 0; i < 5; i++) {
-        printf("Digite o valor de a: ");
-        scanf("%d", &a);
+        printf("Digite um valor: ");
+        scanf("%d", &nn);
 
-        if (a < 0) {
+        if (nn < 0) {
             cn++;
         }
     }
 
     printf("Total de valores negativos: %d", cn);
-
     return 0;
 }
